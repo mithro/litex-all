e0e1f125a5ebaf56f06a6537ccb5891b5d7da06f
Tim 'mithro' Ansell <me@mith.ro>
2020-02-21 12:09:54 -0800

    Fix up trees a bit.

--- .github/trees/trees.py
+++ .github/trees/trees.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os.path
 import setuptools
 import sys
@@ -16,7 +17,7 @@ def import_setup_py(setup_py):
         setuptools.setup = fake_setup
 
         setup_dir = os.path.dirname(setup_py)
-        orig_dir = os.curdir
+        orig_dir = os.path.abspath(os.curdir)
         try:
             os.chdir(setup_dir)
             sys.path.insert(0, '.')
@@ -24,17 +25,19 @@ def import_setup_py(setup_py):
             exec("import setup")
             assert len(MODULES_SETUP) == 1, MODULES_SETUP
             data = list(MODULES_SETUP.items())
+            del sys.modules['setup']
             MODULES_SETUP.clear()
             return data[0]
         finally:
-            os.chdir(orig_dir)
             sys.path.pop(0)
+            os.chdir(orig_dir)
     finally:
         setuptools.setup = setuptools._setup
 
 
 def trees():
-    for d in sorted(os.listdir()):
+    files = sorted(os.listdir())
+    for d in files:
         if not os.path.isdir(d):
             continue
         setup_py = os.path.join(d, 'setup.py')
@@ -47,9 +50,9 @@ def trees():
         src_url = data.get('download_url', src_url)
         # setup(project_urls={'Source':'https://github.com/XXXX'})
         src_url = data.get('project_urls', {}).get('Source', src_url)
-        yield d, name, src_url
+        yield os.path.abspath(d), name, src_url
 
 
 if __name__ == "__main__":
-    for d, src in trees():
-        print(d, "is from", src)
+    for d, name, src in trees():
+        print("{:60s}".format(d), "is", "{:10s}".format(name), "from", src)
