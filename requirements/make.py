ca23fcf2fb896e3864581fb9440c921cd28687c7
Tim 'mithro' Ansell <me@mith.ro>
2020-02-21 11:50:15 -0800

    Fix up the requirements files.

--- requirements/make.py
+++ requirements/make.py
@@ -8,17 +8,19 @@ from litex.soc.cores.cpu import CPUS, CPU_VARIANTS
 
 cpus = [c for c in sorted(CPUS.keys()) if c != "None"]
 
-print("Found", len(cpus))
-pprint.pprint(cpus)
+print("Found", len(cpus), "cpus.")
+print("They are:")
+for c in cpus:
+    print(" *", c)
 
 # Create a ./requirements/XXX.txt for each CPU.
 for c in cpus:
     with open('requirements/{}.txt'.format(c), 'w') as f:
         f.write("""\
 # Install requirements needed to use the {c} soft CPU inside LiteX
--e git+https://github.com/litex-hub/litex-data-{c}.git#egg=litex-data-{c}
+-e git+https://github.com/mithro/litex-data-{c}.git#egg=litex-data-{c}
 # Install the common requirements that LiteX needs
--r ./requirements/base.txt
+-r base.txt
 """.format(c=c))
 
 # Create a ./requirements/all.txt which includes *all* requirements.
@@ -28,7 +30,7 @@ with open('requirements/all.txt', 'w') as f:
         f.write("-r ./requirements/{}.txt\n".format(c))
     f.write("""\
 # Install the common requirements that LiteX needs
--r ./requirements/base.txt
+-r base.txt
 """)
 
 
@@ -46,5 +48,5 @@ with open('requirements/riscv.txt', 'w') as f:
             f.write("-r ./requirements/{}.txt\n".format(c))
     f.write("""\
 # Install the common requirements that LiteX needs
--r ./requirements/base.txt
+-r base.txt
 """)
